5311b30be97c9aa084ea6efbf2a0e256df4bf83a
Claire <felixclaire34@gmail.com>
2024-03-04 13:53:44 +0100

    test ssh git

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// commentaire
 class CategoryController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
